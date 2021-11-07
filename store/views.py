494fa4c566c537ea910efade219f4e01260e9a8c
Robert Kowalczyk <robertkowalczyk011@gmail.com>
2021-11-07 23:48:27 +0100

    Added templates home.html and about.html

--- store/views.py
+++ store/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-# Create your views here.
+
+posts = [
+    {
+        'author': 'Robert',
+        'title': 'AirMax97',
+        'content': 'New brand AirMax97',
+        'date_posted': 'November 07, 2021'
+    },
+    {
+        'author': 'Jarek',
+        'title': 'AirMax95',
+        'content': 'New brand AirMax95',
+        'date_posted': 'November 08, 2021'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'store/home.html', context = context)
+
+def about(request):
+    return render(request, 'store/about.html', context = {'title': 'About'})
